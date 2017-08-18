322606817128c211f8d4aead7bfdddc54894a8af
bnewcomer <bnewcomer@gatech.edu>
2017-08-18 14:27:35 -0700

    implemented rsa challenge and wrote readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # UnifyIDCodingChallenge
+
+author: Benjamin Newcomer
+
+description: This directory contains the source code and test results
+for the UnifyID coding challenge. main.py is a script that contains the
+logic for creating a random bitmap and for generating a random RSA keypair.
+Unfortunately I do not have time to implement the WAV challenge because my
+computer is about to die and I am without a charger close by. I chose to save
+that challenge for last because I am least familiar with audio programming.
+See below for usage. Enjoy!
+
+NOTE: I ran into some problems with the API limits for random.org. I was not able
+to request the number of random bits needed to produce a completely random bitmap
+image because it exceeds the number of bits allowed per day. To remedy this I am
+requesting only one random number per image and using it to seed the python pseudo-random
+number generator. To generate RSA keypairs I am using the Crypto.RSA module, which
+accepts a random number generating function. This function uses the same single number
+request to generate a list of bits. However, it looks like the Crypto.RSA.generate function
+is calling this function more than once, resulting in the depletion of my allowed
+requests per day. I have included test results, but to run the code yourselves, you
+may need to wait a day or use a different API key. Thanks for your understanding!
+
+usage: python main.py [-h] [-b] [-k]
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -b, --bitmap   flag to produce a random bitmap image
+  -k, --keypair  flag to produce a random rsa keypair
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import random
 import json
 from PIL import Image
+from Crypto.PublicKey import RSA
+import argparse
 
 url = "https://api.random.org/json-rpc/1/invoke"
 n = "1"
@@ -10,6 +12,12 @@ maxInt = "256"
 payload = "{\"jsonrpc\": \"2.0\",\n    \"method\": \"generateIntegers\",\n    \"params\": {\n        \"apiKey\": \"f13cae90-1f4d-4904-b9fe-64e33ef5ebdd\",\n        \"n\": " + n + ",\n        \"min\": " + minInt + ",\n        \"max\": " + maxInt + ",\n        \"replacement\": true\n    },\n    \"id\": 42\n}"
 headers = { 'content-type': "application/json" }
 
+# parse command line args
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--bitmap', default=False, action='store_true')
+parser.add_argument('-k', '--keypair', default=False, action='store_true')
+args = parser.parse_args()
+
 # performs a request to get one random number. Random.org
 # imposes a fairly low limit on requests/day, so we will
 # get one number at a time and seed the python pseudo-random
@@ -17,6 +25,13 @@ headers = { 'content-type': "application/json" }
 def getRandom():
     response = requests.request("POST", url, data=payload, headers=headers)
     body = json.loads(response.text)
+
+    # if the API key has exceeded limits
+    # we may get an error here
+    if (not hasattr(body, u'result')):
+        print body[u'error']
+        exit(1)
+
     result = body[u'result']
     random = result[u'random']
     number = random[u'data']
@@ -33,6 +48,11 @@ def generateRandoms(seed, n, min=0, max=256):
         randoms.append(random.randint(min, max))
 
     return randoms
+
+# generate a string of n random bytes
+def generateRandomBytes(seed, n):
+    random.seed(seed)
+    return random.getrandbits(n * 8)
 
 # generate a bitmap image with randomized RGB pixels
 def generateBitmap(width=128, height=128):
@@ -55,7 +75,38 @@ def generateBitmap(width=128, height=128):
 
     return img
 
-# generate bitmap image
-# and save as out.bmp
-image = generateBitmap()
-image.save('out.bmp')
+# generate an RSA key pair seeded with a random
+# number
+def generateRSAKeypair():
+    # accept a length n of bytes and return
+    # a random string with n chars
+    def randfunc(n):
+        return str(generateRandomBytes(getRandom(), n))
+
+    # rsa generate progress function
+    def progress(msg):
+        print 'generating ' + msg
+
+    # generate rsa keypair from using randfunc
+    keypair = RSA.generate(2048, randfunc=randfunc, progress_func=progress)
+    pem = keypair.exportKey('PEM')
+    public = keypair.publickey().exportKey()
+
+    with open('key.pem', 'w') as f:
+        f.write(pem)
+        f.close()
+
+    with open('public', 'w') as f:
+        f.write(public)
+        f.close()
+
+# perform actions based on command
+# line flags
+if args.bitmap:
+    print 'generating random bitmap...'
+    generateBitmap().save('out.bmp')
+    print 'done'
+if args.keypair:
+    print 'generating random rsa keypair'
+    generateRSAKeypair()
+    print 'done'
